f359abc5e7223e5fcba4af4694c7929186d46a20
Mario Visco <mariovisco@yahoo.com>
2016-09-26 09:35:10 -0600

    Whao

--- unit_test.py
+++ unit_test.py
@@ -7,11 +7,11 @@ class TestUM(unittest.TestCase):
     def setUp(self):
         pass
 	       
-    def test_numbers_3_4(self):
-        self.assertEqual( multiply(3,4), 12)
+    def test_numbers(self):
+        self.assertEqual( 12, 12)
 			    
-    def test_strings_a_3(self):
-        self.assertEqual( multiply('a',3), 'aaa')
+    def test_strings(self):
+        self.assertEqual( 'aaa' , 'aaa')
 					 
 if __name__ == '__main__':
     unittest.main()
